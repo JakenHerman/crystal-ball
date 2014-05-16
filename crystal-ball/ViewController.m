14eec16db8d9ec68909272cff4934ac2a0323478
Jaken Herman <JakenHerman@outlook.com>
2014-05-16 06:32:15 -0500

    Update ViewController.m

--- crystal-ball/ViewController.m
+++ crystal-ball/ViewController.m
@@ -18,17 +18,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-<<<<<<< HEAD
-    self.crystalBall = [[TH8Ball alloc] init];
-=======
-    
+     self.crystalBall = [[TH8Ball alloc] init];
+
     self.predictions = [[NSArray alloc] initWithObjects:@"It is certain",
     @"It is decidedly so", @"All signs say Yes", @"The stars are not alligned", 
     @"My reply is no", @"It is doubtful", @"Better not tell you now", 
     @"Concentrate and ask again", @"Unable to answer right now.", nil];
     
     
->>>>>>> FETCH_HEAD
 
 }
 
