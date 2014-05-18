52a928bfec497a7848731d8e47ff91f78b5d4c83
Jaken Herman <JakenHerman@outlook.com>
2014-05-18 17:10:36 -0500

    Update ViewController.m

--- crystal-ball/ViewController.m
+++ crystal-ball/ViewController.m
@@ -8,24 +8,94 @@
 
 #import "ViewController.h"
 #import "TH8Ball.h"
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    SystemSoundID soundEffect;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-     self.crystalBall = [[TH8Ball alloc] init];
-
-    self.predictions = [[NSArray alloc] initWithObjects:@"It is certain",
-    @"It is decidedly so", @"All signs say Yes", @"The stars are not alligned", 
-    @"My reply is no", @"It is doubtful", @"Better not tell you now", 
-    @"Concentrate and ask again", @"Unable to answer right now.", nil];
     
     
+    
+    NSString *soundPath = [[NSBundle mainBundle] pathForResource:@"crystal_ball" ofType:@"mp3"];
+    
+    NSURL *soundURL = [NSURL fileURLWithPath:soundPath];
+    AudioServicesCreateSystemSoundID(CFBridgingRetain(soundURL), &soundEffect);
+    
+    self.crystalBall = [[TH8Ball alloc] init];
+    
+    self.backgroundImageView.animationImages = [[NSArray alloc] initWithObjects:
+                                                [UIImage imageNamed: @"CB00001"],
+                                                [UIImage imageNamed: @"CB00002"],
+                                                [UIImage imageNamed: @"CB00003"],
+                                                [UIImage imageNamed: @"CB00004"],
+                                                [UIImage imageNamed: @"CB00005"],
+                                                [UIImage imageNamed: @"CB00006"],
+                                                [UIImage imageNamed: @"CB00007"],
+                                                [UIImage imageNamed: @"CB00008"],
+                                                [UIImage imageNamed: @"CB00009"],
+                                                [UIImage imageNamed: @"CB00010"],
+                                                [UIImage imageNamed: @"CB00011"],
+                                                [UIImage imageNamed: @"CB00012"],
+                                                [UIImage imageNamed: @"CB00013"],
+                                                [UIImage imageNamed: @"CB00014"],
+                                                [UIImage imageNamed: @"CB00015"],
+                                                [UIImage imageNamed: @"CB00016"],
+                                                [UIImage imageNamed: @"CB00017"],
+                                                [UIImage imageNamed: @"CB00018"],
+                                                [UIImage imageNamed: @"CB00019"],
+                                                [UIImage imageNamed: @"CB00020"],
+                                                [UIImage imageNamed: @"CB00021"],
+                                                [UIImage imageNamed: @"CB00022"],
+                                                [UIImage imageNamed: @"CB00023"],
+                                                [UIImage imageNamed: @"CB00024"],
+                                                [UIImage imageNamed: @"CB00025"],
+                                                [UIImage imageNamed: @"CB00026"],
+                                                [UIImage imageNamed: @"CB00027"],
+                                                [UIImage imageNamed: @"CB00028"],
+                                                [UIImage imageNamed: @"CB00029"],
+                                                [UIImage imageNamed: @"CB00030"],
+                                                [UIImage imageNamed: @"CB00031"],
+                                                [UIImage imageNamed: @"CB00032"],
+                                                [UIImage imageNamed: @"CB00033"],
+                                                [UIImage imageNamed: @"CB00034"],
+                                                [UIImage imageNamed: @"CB00035"],
+                                                [UIImage imageNamed: @"CB00036"],
+                                                [UIImage imageNamed: @"CB00037"],
+                                                [UIImage imageNamed: @"CB00038"],
+                                                [UIImage imageNamed: @"CB00039"],
+                                                [UIImage imageNamed: @"CB00040"],
+                                                [UIImage imageNamed: @"CB00041"],
+                                                [UIImage imageNamed: @"CB00042"],
+                                                [UIImage imageNamed: @"CB00043"],
+                                                [UIImage imageNamed: @"CB00044"],
+                                                [UIImage imageNamed: @"CB00045"],
+                                                [UIImage imageNamed: @"CB00046"],
+                                                [UIImage imageNamed: @"CB00047"],
+                                                [UIImage imageNamed: @"CB00048"],
+                                                [UIImage imageNamed: @"CB00049"],
+                                                [UIImage imageNamed: @"CB00050"],
+                                                [UIImage imageNamed: @"CB00051"],
+                                                [UIImage imageNamed: @"CB00052"],
+                                                [UIImage imageNamed: @"CB00053"],
+                                                [UIImage imageNamed: @"CB00054"],
+                                                [UIImage imageNamed: @"CB00055"],
+                                                [UIImage imageNamed: @"CB00056"],
+                                                [UIImage imageNamed: @"CB00057"],
+                                                [UIImage imageNamed: @"CB00058"],
+                                                [UIImage imageNamed: @"CB00059"],
+                                                [UIImage imageNamed: @"CB00060"],
+                                                nil];
+    
+    self.backgroundImageView.animationDuration = 1.5f;
+    self.backgroundImageView.animationRepeatCount = 1;
 
 }
 
@@ -41,15 +111,46 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)buttonPressed
-{
+#pragma mark - Prediction
+
+- (void) makePrediction {
+    [self.backgroundImageView startAnimating];
+    AudioServicesPlaySystemSound(soundEffect);
+    
+    int i = 0;
+    
+    while (self.backgroundImageView.animationImages == [self.backgroundImageView.animationImages objectAtIndex:i]){
+        self.predictionLabel.text = nil;
+        i++;
+        
+        if (i == 59){
+            break;
+        }
+    }
+    
     self.predictionLabel.text = [self.crystalBall randomPrediction];
 }
 
+- (IBAction)buttonPressed
+{
+    [self makePrediction];
+    
+}
 
+#pragma mark - Motion Events
 
+- (void) motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    self.predictionLabel.text = nil;
+}
 
+- (void) motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    if (motion == UIEventSubtypeMotionShake){
+        [self makePrediction];
+    }
+}
 
-
+- (void) motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    
+}
 
 @end
